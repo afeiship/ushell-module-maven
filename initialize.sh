770fd8c32b2cd4f8ef5d55e8b7a34e7aa9fad779
afeiship <1290657123@qq.com>
2021-05-20 17:37:19 +0800

    wip

--- initialize.sh
+++ initialize.sh
@@ -5,6 +5,7 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 # https://wangxin1248.github.io/life/2020/02/mac-install-maven.html
 
 # auto
+# https://cloud.tencent.com/developer/article/1680711
 brew install maven
 
 unset ROOT_PATH;
